cc9f00f0cf738621595626a30ea8e53e0c992909
Siri <siri.lerdsirisopon@gmail.com>
2022-04-25 22:03:25 +0200

    disabled typescript-eslint/camelcase; removed package-lock.json; specify node and npm version under engines in package.json

--- src/util/cloudinary.ts
+++ src/util/cloudinary.ts
@@ -1,12 +1,13 @@
-import dotenv from 'dotenv';
+/* eslint-disable @typescript-eslint/camelcase */
+import dotenv from 'dotenv'
 
 dotenv.config({ path: '.env' })
 
-const cloudinary = require('cloudinary').v2; 
+const cloudinary = require('cloudinary').v2
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-});
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+})
 
-export { cloudinary };
+export { cloudinary }
